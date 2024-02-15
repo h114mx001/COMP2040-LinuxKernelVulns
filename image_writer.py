781a50853fabb4aac12adbb1ece360853cb0dbf9
h114mx001 <21anh.nt@vinuni.edu.vn>
2024-02-16 00:16:24 +0700

    Update source

--- image_writer.py
+++ image_writer.py
@@ -1,21 +1,40 @@
+import os
 from PIL import Image, ImageDraw, ImageFont
-import argparse
-import random 
-def generate_meme(text: str):
-    # Open the default image
-    img = Image.open('meme.jpg')
+import sys
 
-    font = ImageFont.truetype("arial.ttf", 35)
-    # Create ImageDraw object
-    draw = ImageDraw.Draw(img)
-    randomx = random.randint(0, img.size[0])
-    randomy = random.randint(0, img.size[1])
-    draw.text((randomx, randomy), text, fill=(255, 0, 0), font=font)
-    # Save the image in a new file
-    img.save('meme_with_text.jpg')
+font = ImageFont.truetype('comic-sans.ttf', size=48)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Generate a meme with text.')
-    parser.add_argument('-t', '--text', type=str, required=True, help='The text to add to the meme.')
-    args = parser.parse_args()
-    generate_meme(args.text)
+text = sys.argv[1]
+
+if len(text) > 1000: 
+  text = "Too long!"
+
+width, height = 512, 562
+img = Image.new('RGB', (width, height), color=(255, 255, 255))
+canvas = ImageDraw.Draw(img)
+chunks = []
+
+chunk = ""
+# print(canvas.textlength(text, font=font))
+for char in text:
+  chunk += char
+  text_width = canvas.textlength(chunk, font=font) 
+  if text_width >= (width-20):
+    chunks.append(chunk[:-1])
+    chunk = char
+
+if len(chunks) == 0:
+  chunks.append(chunk)
+
+if chunks[-1] != chunk:
+  chunks.append(chunk)
+  
+for i,chunk in enumerate(chunks):
+  text_width = canvas.textlength(chunk, font=font)
+  x_pos = int((width - text_width) / 2) + 10
+  y_pos = 15 + i * 100
+  canvas.text((x_pos, y_pos), chunk, font=font, fill='#000000')
+
+img2 = Image.open('static/VinMeme-Small.png')
+img.paste(img2, (0, 0), img2.convert('RGBA'))
+img.save("meme_with_text.png")
--- main.py
+++ main.py
@@ -6,16 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return open("index.html").read()
 
 @app.route("/meme", methods=['POST'])
 def generate_meme():
     # Get the text from the form submission
-    text = request.form.get('inputText')
-    os.system(f"python3 ./image_writer.py -t {text}")    
+    text = request.form.get('text')
+    # print(text)
+    os.system(f"python3 ./image_writer.py {text}")    
     # subprocess.check_output(f"python3 ./image_writer.py -t {text}", shell=True)
     # Return the new image
-    return send_file('meme_with_text.jpg', mimetype='image/jpeg')
+    return send_file('meme_with_text.png', mimetype='image/jpeg')
 
 if __name__ == "__main__":
     app.run(debug = False, port=5000, host="0.0.0.0")
